c3e14e0e1fa39942a2fddc45b7b247b8961683c9
Pete Stephens <hzqmrk@gmail.com>
2021-03-21 20:20:36 -0400

    It works (both LEDs on)

--- led.c
+++ led.c
@@ -1,12 +1,14 @@
 #include <cc2530.h>
 
 void main() {
-   P0SEL &= ~0x01;
-   P1SEL &= ~0x02;
+   while(1) {
+      P0SEL &= ~0x01;
+      P1SEL &= ~0x02;
 
-   P0DIR |= ~0x01;
-   P1DIR |= ~0x02;
+      P0DIR |= 0x01;
+      P1DIR |= 0x02;
 
-   P0 &= ~0x01;
-   P1 |= 0x02;
+      P0 &= ~0x01;
+      P1 |= 0x02;
+   }
 }
